a6e66e6588fe1670238446e49ff0f3084bea8e7a
Jack-Sh <78345769+Jack-Sh@users.noreply.github.com>
2022-05-18 09:52:01 +1200

    v2.5 complete and final looping component complete

--- 01_looping_v3.py
+++ 01_looping_v3.py
@@ -2,46 +2,89 @@
 
 
 # Checks for an integer more than 0
-def int_check(question):
+def int_check(question=None, num=None):
 
+    # custom error message
     error = "Please enter a whole number that is more than 0\n"
 
+    # define situation
+    situation = ""
+
+    # if user has specified a question and not a number
+    # then the situation is question only
+    if question is not None and num is None:
+        situation = "question only"
+
+    # if user has specified number and not question
+    # then the situation is number only
+    elif question is None and num is not None:
+        situation = "num only"
+
+    # start the loop
     valid = False
     while not valid:
 
-        # ask user for number and check it is valid
         try:
-            response = int(input(question))
+            # if the situation is question only ask for number
+            if situation == "question only":
 
-            # if the response is zero or lower print an error
-            if response <= 0:
-                print(error)
+                response = int(input(question))
 
-            # if the response is greater than zero, the code continues
+                # if the response is zero or lower print an error
+                if response <= 0:
+                    print(error)
+
+                # if the response is greater than zero, the code continues
+                else:
+                    return response
+
+            # if the situation is number only
             else:
-                return response
+                # change the string to an integer
+                num = int(num)
+                # if the response is zero or lower print an error
+                if num <= 0:
+                    print(error)
+
+                # if the response is greater than zero, the code continues
+                else:
+                    return num
 
         # if an integer is not entered, display an error
         except ValueError:
             print(error)
 
 
-# main routine
+# Main routine
+
+# ask how many question the user needs to answer
 questions_needed = input("How many questions do you need to answer? ")
 
+# if user types [enter] set questions to 1000 (for now)
 if questions_needed == "":
     questions_needed = 1000
 
+# if users enters a number check that it's valid
 else:
-    int_check(questions_needed)
+    int_check(None, questions_needed)
 
+# program begins
 print("\n---- Program Launches ----")
 print()
 
+# define number of questions and ensure that questions needed is a integer
 num_questions = int(questions_needed)
 
+# start loop (while number of questions does not = 0)
 while num_questions != 0:
 
+    # ask for side 1
     side_1 = input("Side 1? ")
     print()
+
+    # if user enters xxx break the loop
+    if side_1 == "xxx":
+        break
+
+    # subtract 1 from the questions counter
     num_questions -= 1
