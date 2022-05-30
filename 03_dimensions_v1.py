b962d3a4ac4812ecce75ce6afd0875407728642f
Jack-Sh <78345769+Jack-Sh@users.noreply.github.com>
2022-05-31 10:58:35 +1200

    dimesnions done

--- 03_dimensions_v1.py
+++ 03_dimensions_v1.py
@@ -1,6 +1,60 @@
 # functions
 
 
+# Checks for an integer more than 0
+def int_check(question=None, num=None):
+
+    # custom error message
+    error = "Please enter a whole number that is more than 0\n"
+
+    # define situation
+    situation = ""
+
+    # if user has specified a question and not a number
+    # then the situation is question only
+    if question is not None and num is None:
+        situation = "question only"
+
+    # if user has specified number and not question
+    # then the situation is number only
+    elif question is None and num is not None:
+        situation = "num only"
+
+    # start the loop
+    valid = False
+    while not valid:
+
+        try:
+            # if the situation is question only ask for number
+            if situation == "question only":
+
+                response = int(input(question))
+
+                # if the response is zero or lower print an error
+                if response <= 0:
+                    print(error)
+
+                # if the response is greater than zero, the code continues
+                else:
+                    return response
+
+            # if the situation is number only
+            else:
+                # change the string to an integer
+                num = int(num)
+                # if the response is zero or lower print an error
+                if num <= 0:
+                    print(error)
+
+                # if the response is greater than zero, the code continues
+                else:
+                    return num
+
+        # if an integer is not entered, display an error
+        except ValueError:
+            print(error)
+
+
 # Checks that the response is in the given list
 def choice_checker(question, valid_list, error, ):
 
@@ -32,23 +86,23 @@ valid_shapes = ["square", "rectangle", "circle", "triangle"]
 for item in range(0,4):
 
     # get shape and check that it's valid
-    print()
     what_shape = choice_checker("Shape? ", valid_shapes, "Please enter a valid shape\n")
-    print()
 
-    if what_shape == "square":
-        print("-- Square --")
-        dimension_1 = int(input("Enter a side: "))
-    elif what_shape == "rectangle":
-        print("-- Rectangle --")
-        dimension_1 = int(input("Enter the base length: "))
-        dimension_2 = int(input("Enter the height length: "))
-    elif what_shape == "circle":
-        print("-- Circle --")
-        dimension_1 = int(input("Enter the radius: "))
+    if what_shape == "rectangle" or what_shape == "square":
+
+        base = int_check("Enter Base Length: ", None)
+        height = int_check("Enter Height Length: ", None)
+        print("Base = {} | Height = {} ".format(base, height))
+        print()
+
+    elif what_shape == "triangle":
+
+        base = int_check("Enter Base Length: ", None)
+        height = int_check("Enter Height Length: ", None)
+        side_2 = int_check("Enter Side 2 Length: ", None)
+        side_3 = int_check("Enter Side 3 Length: ", None)
+        print("Base = {} | Height = {} | Side 2 = {} | Side 3 = {}".format(base, height, side_2, side_3))
+
     else:
-        print("-- Triangle --")
-        dimension_1 = int(input("Enter the base length: "))
-        dimension_2 = int(input("Enter the height length: "))
-        dimension_3 = int(input("Enter the side 2 length: "))
-        dimension_4 = int(input("Enter the side 3 length: "))
+        radius = int_check("Enter Radius: ", None)
+        print("Radius {}".format(radius))
