da91ba558719a347c95e242a1b165e2928751280
Jack-Sh <78345769+Jack-Sh@users.noreply.github.com>
2022-07-04 11:39:58 +1200

    integrated into base

--- 00_APC_Base_v2.py
+++ 00_APC_Base_v2.py
@@ -1,60 +1,33 @@
 import math
 
+# setup summary list for do_math function
+summary_list = []
+
 # Functions
 
 
-# Checks for an integer more than 0
-def int_check(question=None, num=None):
+def int_check(question, error, num_type):
+    while True:
 
-    # custom error message
-    error = "Please enter a whole number that is more than 0\n"
+        response = input(question)
 
-    # define situation
-    situation = ""
+        if response != "":
 
-    # if user has specified a question and not a number
-    # then the situation is question only
-    if question is not None and num is None:
-        situation = "question only"
+            try:
+                response = num_type(response)
 
-    # if user has specified number and not question
-    # then the situation is number only
-    elif question is None and num is not None:
-        situation = "num only"
-
-    # start the loop
-    valid = False
-    while not valid:
-
-        try:
-            # if the situation is question only ask for number
-            if situation == "question only":
-
-                response = int(input(question))
-
-                # if the response is zero or lower print an error
                 if response <= 0:
                     print(error)
+                    continue
 
-                # if the response is greater than zero, the code continues
                 else:
                     return response
 
-            # if the situation is number only
-            else:
-                # change the string to an integer
-                num = int(num)
-                # if the response is zero or lower print an error
-                if num <= 0:
-                    print(error)
+            except ValueError:
+                print(error)
+                continue
 
-                # if the response is greater than zero, the code continues
-                else:
-                    return num
-
-        # if an integer is not entered, display an error
-        except ValueError:
-            print(error)
+        return response
 
 
 # Checks that the response is in the given list
@@ -73,95 +46,112 @@ def choice_checker(question, valid_list, error, ):
 
         for item in valid_list:
             if response == item[0] or response == item:
-                print(item)
                 return item
 
         else:
             print(error)
 
 
-# Ask user for shape and does math
+# Ask user for shape, do math then create the output for summary
 def do_math():
 
     what_shape = choice_checker("Shape? ", valid_shapes, "Please enter a valid shape\n")
     print()
 
-    # get dimensions
+    # get dimensions and do math
 
     if what_shape == "square":
-        base = int_check("Enter a side: ")
-        height = base
+        base = int_check("Enter a side: ", "Please enter a number more than 0", float)
+
+        # --- area ---
+        area = base * base
+        # --- perimeter ---
+        perimeter = 4 * base
+
+        summary = "Question {}: Shape: Square | A side: {}\n" \
+                  "Area = {} | Perimeter = {}\n".format(num_questions + 1, base, area, perimeter)
 
     elif what_shape == "rectangle":
-        base = int_check("Base: ")
-        height = int_check("Height: ")
+        base = int_check("Base: ", "Please enter a number more than 0", float)
+        height = int_check("Height: ", "Please enter a number more than 0", float)
 
-    elif what_shape == "circle":
-        radius = int_check("Radius: ")
-
-    else:
-        base = int_check("Base: ")
-        height = int_check("Height: ")
-        side_2 = int_check("Side 2: ")
-        side_3 = int_check("Side 3: ")
-
-    # do math
-
-    # area
-    if what_shape == "square" or what_shape == "rectangle":
+        # --- area ---
         area = base * height
-
-    elif what_shape == "triangle":
-        area = 0.5 * base * height
-
-    else:
-        area = math.pi * radius * radius
-
-    # perimeter
-    if what_shape == "square" or what_shape == "rectangle":
+        # --- perimeter ---
         perimeter = 2 * base + 2 * height
 
-    elif what_shape == "triangle":
-        perimeter = base + side_2 + side_3
+        summary = "Question {}: Shape: Rectangle | Base: {}, Height: {}\n" \
+                  "Area = {} | Perimeter = {}\n".format(num_questions + 1, base, height, area, perimeter)
 
-    else:
+    elif what_shape == "circle":
+        radius = int_check("Radius: ", "Please enter a number more than 0", float)
+
+        # --- area ---
+        area = math.pi * radius * radius
+        # --- perimeter ---
         perimeter = 2 * math.pi * radius
 
-    # print output
-    print("Area = {} | Perimeter = {}".format(area, perimeter))
-    print()
+        summary = "Question {}: Shape: Circle | Radius: {}\n" \
+                  "Area = {} | Perimeter = {}\n".format(num_questions + 1, radius, area, perimeter)
 
-    return area, perimeter
+    else:
+        base = int_check("Base: ", "Please enter a number more than 0", float)
+        height = int_check("Height: ", "Please enter a number more than 0", float)
+        side_2 = int_check("Side 2: ", "Please enter a number more than 0", float)
+        side_3 = int_check("Side 3: ", "Please enter a number more than 0", float)
 
+        # --- area ---
+        area = 0.5 * base * height
+        # --- perimeter ---
+        perimeter = base + side_2 + side_3
 
-# Main Routine...
+        summary = "Question {}: Shape: Triangle | Base: {}, Height: {}, Side 2: {}, Side 3: {}\n" \
+                  "Area = {} | Perimeter = {}\n".format(num_questions + 1, base, height, side_2, side_3, area, perimeter)
+
+    # print summary after every question
+    quick_summary = "Area = {} | Perimeter = {}\n".format(area, perimeter)
+    print(quick_summary)
+
+    summary_list.append(summary)
+
+    return ""
+
 
 # setup list
 valid_shapes = ["square", "rectangle", "circle", "triangle"]
+yes_no_list = ["yes", "no"]
+num_questions = 0
 
 # Ask how many question the user needs to answer
-questions_needed = input("How many questions do you need to answer? ")
+questions_needed = int_check("How many questions do you need to answer? ", "Please enter a num more than 0 or [enter]",
+                             int)
 
-# If user types [enter] set questions to 1000 (for now)
 if questions_needed == "":
     questions_needed = 1000
-
-# If users enters a number check that it's valid
-else:
-    int_check(None, questions_needed)
 
 # Program Begins
 print("\n---- Program Launches ----")
 print()
 
-# Define number of questions and ensure that questions needed is a integer
-num_questions = int(questions_needed)
 
-# Start loop (while number of questions does not = 0)
-while num_questions != 0:
+# Start loop (while the number of questions doesn't equal the max questions)
+while num_questions != questions_needed:
+
+    # print the question number
+    print("--- Question: {} ---".format(num_questions + 1))
 
     # ask user for shape and do the math
     do_math()
 
-    # Subtract 1 from the questions counter
-    num_questions -= 1
+    # add one to question counter
+    num_questions += 1
+
+# summary
+see_summary = choice_checker("Would you like to see the summary? ", yes_no_list, "Please enter yes or no ")
+
+# if the user wants to see the summary print it
+if see_summary == "yes":
+
+    print()
+    for summary in summary_list:
+        print(summary)
