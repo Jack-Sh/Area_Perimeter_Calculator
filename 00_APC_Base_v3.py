61d9f48a9d2016d11009e8d36ef4e6337cbf63bd
Jack-Sh <78345769+Jack-Sh@users.noreply.github.com>
2022-07-25 11:12:51 +1200

    fixed all issues doing final testing

--- 00_APC_Base_v3.py
+++ 00_APC_Base_v3.py
@@ -4,23 +4,38 @@ import pandas
 # Functions
 
 
-def int_check(question, error, num_type):
+# rounds to 2dp
+def rounding(x):
+    return "{:.2f}".format(x)
+
+
+# checks if a number is valid
+def num_check(question, num_type):
+    error = "Please enter a number more than 0"
+
+    # start loop
     while True:
 
+        # ask question
         response = input(question)
 
+        # if the response does not equal [enter]
         if response != "":
 
+            # ask question with given num_type (float, int)
             try:
                 response = num_type(response)
 
+                # if the response is less than or equal to zero print error
                 if response <= 0:
                     print(error)
                     continue
 
+                # otherwise, return the response
                 else:
                     return response
 
+            # print error for unexpected values and restart the loop
             except ValueError:
                 print(error)
                 continue
@@ -51,12 +66,12 @@ def choice_checker(question, valid_list, error, ):
 
 
 # Ask user for shape, do math then create the output for summary
-def do_math(shape):
+def do_math(shape, rounded_answers):
 
     # get dimensions and do math
 
     if shape == "square":
-        side_1 = int_check("Enter a side: ", "Please enter a number more than 0", float)
+        side_1 = num_check("Enter a side: ", float)
 
         # --- area ---
         area = side_1 * side_1
@@ -68,8 +83,8 @@ def do_math(shape):
         side_3 = "/"
 
     elif shape == "rectangle":
-        side_1 = int_check("Base: ", "Please enter a number more than 0", float)
-        side_2 = int_check("Height: ", "Please enter a number more than 0", float)
+        side_1 = num_check("Base: ", float)
+        side_2 = num_check("Height: ", float)
 
         # --- area ---
         area = side_1 * side_2
@@ -80,7 +95,7 @@ def do_math(shape):
         side_3 = "/"
 
     elif shape == "circle":
-        side_1 = int_check("Radius: ", "Please enter a number more than 0", float)
+        side_1 = num_check("Radius: ", float)
 
         # --- area ---
         area = math.pi * side_1 * side_1
@@ -92,17 +107,46 @@ def do_math(shape):
         side_3 = "/"
 
     else:
-        side_1 = int_check("Side 1: ", "Please enter a number more than 0", float)
-        side_2 = int_check("Side 2: ", "Please enter a number more than 0", float)
-        side_3 = int_check("Side 3: ", "Please enter a number more than 0", float)
+        check_dimensions = choice_checker("Do you just have base and height? ", yes_no_list, "Please enter yes or no\n")
+        print()
 
-        # --- perimeter ---
-        perimeter = side_1 + side_2 + side_3
+        side_1 = 0
+        side_2 = 0
+        side_3 = 1
 
-        # --- area ---
-        semi_perim = perimeter/2
-        area_unsquared = (semi_perim - side_1) * (semi_perim - side_2) * (semi_perim - side_3) * semi_perim
-        area = math.sqrt(area_unsquared)
+        if check_dimensions == "no":
+
+            while side_1 + side_2 <= side_3:
+
+                side_1 = num_check("Side 1: ", float)
+                side_2 = num_check("Side 2: ", float)
+                side_3 = num_check("Side 3: ", float)
+
+                if side_1 + side_2 <= side_3:
+                    print("This is an impossible triangle. The sum of side 1 + side 2 must be greater than side 3\n")
+                    continue
+
+            # --- perimeter ---
+            perimeter = side_1 + side_2 + side_3
+
+            # --- area ---
+            semi_perim = perimeter/2
+            area_unsquared = (semi_perim - side_1) * (semi_perim - side_2) * (semi_perim - side_3) * semi_perim
+            area = math.sqrt(area_unsquared)
+
+        else:
+            side_1 = num_check("Base: ", float)
+            side_2 = num_check("Height: ", float)
+            side_3 = "/"
+
+            # --- area ---
+            area = 0.5 * side_1 * side_2
+
+            perimeter = "/"
+
+    if rounded_answers == "yes":
+        area = rounding(area)
+        perimeter = rounding(perimeter)
 
     # append all data into respective lists
     all_side_1.append(side_1)
@@ -131,7 +175,7 @@ def instructions():
 
 
 # setup lists
-valid_shapes = ["square", "rectangle", "circle", "triangle"]
+valid_shapes = ["square", "rectangle", "circle", "triangle", "xxx"]
 yes_no_list = ["yes", "no"]
 
 all_shapes = []
@@ -161,9 +205,11 @@ print()
 if see_instructions == "yes":
     instructions()
 
+do_rounding = choice_checker("Do you want rounded answers? ", yes_no_list, "Please enter yes or no\n")
+print()
+
 # Ask how many question the user needs to answer
-questions_needed = int_check("How many questions do you need to answer? ",
-                             "Please enter a whole number more than 0 or [enter]\n", int)
+questions_needed = num_check("How many questions do you need to answer? ", int)
 
 if questions_needed == "":
     questions_needed = 1000
@@ -179,16 +225,23 @@ while num_questions != questions_needed:
     # print the question number
     print("--- Question: {} ---".format(num_questions + 1))
 
+    # ask user for shape
     what_shape = choice_checker("Shape? ", valid_shapes, "Please enter a valid shape (square, rectangle, circle, triangle)\n")
     print()
 
-    all_shapes.append(what_shape)
-
+    # if user enters xxx break loop
     if what_shape == "xxx":
         break
 
-    # ask user for shape and do the math
-    do_math(what_shape)
+    # add shape to list
+    all_shapes.append(what_shape)
+
+    # call the function and set round answers to yes or no
+    if do_rounding == "yes":
+        do_math(what_shape, "yes")
+
+    else:
+        do_math(what_shape, "no")
 
     # add one to question counter
     num_questions += 1
